2c2f29f3223e019cfc10b6d76d674fa1e0a26c06
wanderer <mbecze@craveprogramming.com>
2014-06-27 17:34:38 -0400

    added block tests

--- test/block.js
+++ test/block.js
@@ -30,4 +30,26 @@ describe('[Block]: Basic functions', function () {
             assert(block.header.validatePOW());
         });
     });
+
+    it('should validate difficulty', function () {
+        //the genesis block does not have a valid POW
+        blocks.forEach(function (block, i) {
+            assert(block.header.validateDifficulty(new Block(blockFixtures[i].block)));
+        });
+    });
+
+    it('should validate the whole blockheader', function () {
+        //the genesis block does not have a valid POW
+        blocks.forEach(function (block, i) {
+            assert(block.header.validate(new Block(blockFixtures[i].block)));
+        });
+    });
+
+    it('should validate the whole block', function () {
+        //the genesis block does not have a valid POW; note we have no uncles
+        //so we don't need the parnet's parent
+        blocks.forEach(function (block, i) {
+            assert(block.validate(new Block(blockFixtures[i].block)));
+        });
+    });
 });
