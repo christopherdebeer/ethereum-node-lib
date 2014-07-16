be42564e1bdfdf037efa3b2f51cf60541e1ed2f0
wanderer <mbecze@craveprogramming.com>
2014-07-16 13:50:50 -0400

    fixed transaction validation

--- bin/ethereum.js
+++ bin/ethereum.js
@@ -1,47 +1,47 @@
 #!/usr/bin/env node
 
 var Ethereum = require('../'),
-  genesis = require('./genesis'),
-  networking = require('./networking'),
-  levelup = require('levelup'),
-  async = require('async');
+    genesis = require('./genesis'),
+    networking = require('./networking'),
+    levelup = require('levelup'),
+    async = require('async');
 
 var internals = {},
-  Blockchain = Ethereum.Blockchain,
-  State = Ethereum.State,
-  Utils = Ethereum.Utils,
-  stateDB = levelup('./db/state'),
-  blockDB = levelup('./db/block'),
-  detailsDB = levelup('./db/details');
+    Blockchain = Ethereum.Blockchain,
+    State = Ethereum.State,
+    Utils = Ethereum.Utils,
+    stateDB = levelup('./db/state'),
+    blockDB = levelup('./db/block'),
+    detailsDB = levelup('./db/details');
 
 var argv = require('minimist')(process.argv.slice(2));
 if (argv.help) {
-  console.log('TODO: write help');
+    console.log('TODO: write help');
 } else {
-  internals.blockchain = new Blockchain(blockDB, detailsDB);
-  internals.state = new State(stateDB);
+    internals.blockchain = new Blockchain(blockDB, detailsDB);
+    internals.state = new State(stateDB);
 
-  internals.blockchain.init(function () {
-    if (!internals.blockchain.head) {
-      console.log('generating genesis block');
-      genesis.init(stateDB, internals.blockchain);
-    }
-    if (!argv.dumpbc) {
-      networking.init(internals.blockchain, internals.state);
-    } else {
-      var hash = internals.blockchain.meta.genesis,
-        height = internals.blockchain.meta.height;
+    internals.blockchain.init(function () {
+        if (!internals.blockchain.head) {
+            console.log('generating genesis block');
+            genesis.init(stateDB, internals.blockchain);
+        }
+        if (!argv.dumpbc) {
+            networking.init(internals.blockchain, internals.state);
+        } else {
+            var hash = internals.blockchain.meta.genesis,
+                height = internals.blockchain.meta.height;
 
-      internals.blockchain.getBlockChain([hash], height , function (err, results) {
-        //add the genesis block to the end of the results
-        internals.blockchain.getBlock(hash, function (err, genesis) {
-          results.push(genesis);
-          results = results.map(function (b) {
-            return b.serialize(false);
-          });
-          console.log(Utils.bufferToJSON(results));
-        });
-      });
-    }
-  });
+            internals.blockchain.getBlockChain([hash], height, function (err, results) {
+                //add the genesis block to the end of the results
+                internals.blockchain.getBlock(hash, function (err, genesis) {
+                    results.push(genesis);
+                    results = results.map(function (b) {
+                        return b.serialize(false);
+                    });
+                    console.log(Utils.bufferToJSON(results));
+                });
+            });
+        }
+    });
 }
--- bin/networking.js
+++ bin/networking.js
@@ -1,82 +1,83 @@
 var async = require('async'),
-  Ethereum = require('../'),
-  genesis = require('./genesis');
+    Ethereum = require('../'),
+    genesis = require('./genesis');
 
 var internals = {},
-  Network = Ethereum.Network;
+    Network = Ethereum.Network;
+    Utils = Ethereum.Utils;
 
 
 exports.init = function (blockchain, state) {
-  console.log('starting networking');
-  internals.network = new Network();
-  internals.blockchain = blockchain;
-  internals.state = state;
+    console.log('starting networking');
+    internals.network = new Network();
+    internals.blockchain = blockchain;
+    internals.state = state;
 
-  internals.network.on('connecting', function (socket, port, host) {
-    console.log('[networking]' + host + ':' + port + ' connecting');
-  });
-
-  internals.network.on('closing', function (peer) {
-    console.log('[networking]' + peer.internalId + ' closing');
-  });
-
-  internals.network.on('message.hello', function (hello, peer) {
-    console.log('[networking] ' + hello.ip + ':' + hello.port + ' hello');
-    internals.sync(peer, blockchain.head.hash(), function (err) {
-      console.log('done syncing');
+    internals.network.on('connecting', function (socket, port, host) {
+        console.log('[networking]' + host + ':' + port + ' connecting');
     });
 
-  });
+    internals.network.on('closing', function (peer) {
+        console.log('[networking]' + peer.internalId + ' closing');
+    });
 
-  internals.network.on('message.transactions', function (transactions, peer) {
-    console.log('[networking]' + peer.internalId + ' got transactions');
-    //TODO: check if transaction is in the DB
-    //check if the transaction is valid
-    //push tx to txlist
-    //save in db
-  });
+    internals.network.on('message.hello', function (hello, peer) {
+        console.log('[networking] ' + hello.ip + ':' + hello.port + ' hello');
+        internals.sync(peer, blockchain.head.hash(), function (err) {
+            console.log('done syncing');
+        });
 
-  internals.network.on('message.peers', function (peers, peer) {
-    console.log(peers);
-    console.log('[networking]' + peer.internalId + ' got peers');
-  });
+    });
 
-  internals.network.on('message.getPeers', function (peers, peer) {
-    console.log('[networking]' + peer.internalId + ' got get peers');
-  });
+    internals.network.on('message.transactions', function (transactions, peer) {
+        console.log('[networking]' + peer.internalId + ' got transactions');
+        //TODO: check if transaction is in the DB
+        //check if the transaction is valid
+        //push tx to txlist
+        //save in db
+    });
 
-  internals.network.on('message.blocks', function (blocks, peer) {
-    console.log('[networking]' + peer.internalId + ' got blocks');
-    internals.onBlock(blocks);
-  });
+    internals.network.on('message.peers', function (peers, peer) {
+        console.log(peers);
+        console.log('[networking]' + peer.internalId + ' got peers');
+    });
 
-  internals.network.on('message.getChain', function (message, peer) {
-    console.log(message);
-    console.log('[networking]' + peer.internalId + ' got get chain');
-  });
+    internals.network.on('message.getPeers', function (peers, peer) {
+        console.log('[networking]' + peer.internalId + ' got get peers');
+    });
 
-  internals.network.on('message.notInChain', function (message, peer) {
-    console.log('[networking]' + peer.internalId + ' got not in chain');
-  });
+    internals.network.on('message.blocks', function (blocks, peer) {
+        console.log('[networking]' + peer.internalId + ' got blocks');
+        internals.onBlock(blocks);
+    });
 
-  internals.network.on('message.getTransactions', function (message, peer) {
-    console.log('[networking]' + peer.internalId + ' got request for transactions');
-  });
+    internals.network.on('message.getChain', function (message, peer) {
+        console.log(message);
+        console.log('[networking]' + peer.internalId + ' got get chain');
+    });
 
-  internals.network.on('message.disconnect', function (message, peer) {
-    console.log('[networking]' + peer.internalId + ' got disconnected:' + message.reason);
-  });
+    internals.network.on('message.notInChain', function (message, peer) {
+        console.log('[networking]' + peer.internalId + ' got not in chain');
+    });
 
-  internals.network.on('socket.error', function (e) {
-    console.log('[networking] socket error: ' + e);
-  });
+    internals.network.on('message.getTransactions', function (message, peer) {
+        console.log('[networking]' + peer.internalId + ' got request for transactions');
+    });
 
-  internals.network.on('parsing.error', function (e) {
-    console.log('[networking] parse error: ' + e);
-  });
+    internals.network.on('message.disconnect', function (message, peer) {
+        console.log('[networking]' + peer.internalId + ' got disconnected:' + message.reason);
+    });
 
-  internals.network.listen(30303, '0.0.0.0');
-  //internals.network.connect(30303, '54.204.10.41');
+    internals.network.on('socket.error', function (e) {
+        console.log('[networking] socket error: ' + e);
+    });
+
+    internals.network.on('parsing.error', function (e) {
+        console.log('[networking] parse error: ' + e);
+    });
+
+    internals.network.listen(30303, '0.0.0.0');
+    //internals.network.connect(30303, '54.204.10.41');
 
 };
 
@@ -89,49 +90,49 @@ exports.init = function (blockchain, state) {
  * @param {Function} cb - the callback
  */
 internals.sync = function (peer, startHash, cb) {
-  var more = true,
-    count = 30; //how mainly blocks to get.
+    var more = true,
+        count = 30; //how mainly blocks to get.
 
-  //get the first five hashes
-  internals.blockchain.getBlockHashes(startHash, -5, function (err, hashes) {
-    async.whilst(function () {
-      return more;
-    }, function (cb2) {
-      //include the strating hash
-      hashes.unshift(startHash);
-      var onMessage = function (msgType, data) {
-        if (msgType === 'blocks' || msgType === 'notInChain') {
-          peer.removeListener('message', onMessage);
-        }
+    //get the first five hashes
+    internals.blockchain.getBlockHashes(startHash, -5, function (err, hashes) {
+        async.whilst(function () {
+            return more;
+        }, function (cb2) {
+            //include the strating hash
+            hashes.unshift(startHash);
+            var onMessage = function (msgType, data) {
+                if (msgType === 'blocks' || msgType === 'notInChain') {
+                    peer.removeListener('message', onMessage);
+                }
 
-        if (msgType === 'blocks') {
-          if (data.length !== count) {
-            more = false;
-          }
+                if (msgType === 'blocks') {
+                    if (data.length !== count) {
+                        more = false;
+                    }
 
-          cb2();
-        } else if (msgType === 'notInChain') {
-          //fetch the last 
-          peer.once('message.notInChain', function () {
-            if (internals.blockchain.genesisHash.toString('hex') === hashes[0]) {
-              //wrong genesis block
-              peer.sendDisconnect(0x06, cb2);
+                    cb2();
+                } else if (msgType === 'notInChain') {
+                    //fetch the last 
+                    peer.once('message.notInChain', function () {
+                        if (internals.blockchain.genesisHash.toString('hex') === hashes[0]) {
+                            //wrong genesis block
+                            peer.sendDisconnect(0x06, cb2);
+                        } else {
+                            //keep trying to synce. Start with the oldest hash
+                            internals.sync(peer, hashes.pop(), cb2);
+                        }
+                    });
+                }
+            };
+
+            if (err) {
+                cb2(err);
             } else {
-              //keep trying to synce. Start with the oldest hash
-              internals.sync(peer, hashes.pop(), cb2);
+                peer.on('message', onMessage);
+                peer.sendGetChain(hashes, count);
             }
-          });
-        }
-      };
-
-      if (err) {
-        cb2(err);
-      } else {
-        peer.on('message', onMessage);
-        peer.sendGetChain(hashes, count);
-      }
-    }, cb);
-  });
+        }, cb);
+    });
 };
 
 /**
@@ -139,23 +140,22 @@ internals.sync = function (peer, startHash, cb) {
  * @method onBlock
  */
 internals.onBlock = function (blocks) {
-  console.log('added block');
-  blocks.forEach(function (block) {
-    //TODO: get the parent block root state if parent is not head
-    //validate block here -->
-
-    //proccess the block and  update the world state
-    internals.state.processBlock(block, function (err) {
-      if (!err) {
-        internals.blockchain.addBlock(block, function (err) {
-          //probably couldn't find the block
-          if (err) {
-            console.log('[blockchain] error: ' + err);
-          }
+    console.log('added block');
+    blocks.forEach(function (block) {
+        //TODO: get the parent block root state if parent is not head
+        //validate block here -->
+        //proccess the block and  update the world state
+        internals.state.processBlock(block, function (err) {
+            if (!err) {
+                internals.blockchain.addBlock(block, function (err) {
+                    //probably couldn't find the block
+                    if (err) {
+                        console.log('[blockchain] error: ' + err);
+                    }
+                });
+            } else {
+                console.log('[state] error ' + err);
+            }
         });
-      } else {
-        console.log('[state] error ' + err);
-      }
     });
-  });
 };
